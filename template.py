95200d58ff9219bdf0fbc7410cc839cf0e33790e
Mudit Dubey <mudit.dubey@aspectratioai.com>
2024-08-01 12:02:11 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name= "textSummarizer"
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/v/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb"
+    # add more files if requires like test files.
+]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename= os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file {filename}")
+
+    if ( not os.path.exists(filepath)) or (os.path.getsize(filepath)== 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Created empty file: {filepath}")
+    else:
+        logging.info(f"File {filepath} already exists.")
